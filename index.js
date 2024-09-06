bdf1de1f6d2acfc0f38a2d961171c2750358a500
shenkoshenkoshenko <shenkoshenkoshenko@gmail.com>
2024-09-06 18:26:31 -0400

    many elements added

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-async function getPollution () {
+async function getPollutionURL () {
     try {
       const response = await fetch('https://api.websitecarbon.com/site?url=https%3A%2F%2Fwww.github.com%2F');
       if (!response.ok) {
         throw new Error(response.status);
       }
       const data = await response.json();
-      console.log(`${data.url} produces ${data.statistics.co2.grid.grams} grams of CO2 per page load`);
+      return data.url;
     } catch (error) {
       console.error(error);
     }
   }
 
-  getPollution()
+  getPollutionURL()
