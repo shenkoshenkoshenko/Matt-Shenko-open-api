8181310ea327bb4256bc4dbb87ee79e8c1d2e927
shenkoshenkoshenko <shenkoshenkoshenko@gmail.com>
2024-09-10 15:24:10 -0400

    added some functionality, CORS needs correcting

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-async function getPollutionURL () {
+const urlInput = document.getElementById('url');
+const fetchButton = document.getElementById('fetch');
+const resetButton = document.getElementById('reset')
+const outputMessage = document.getElementById('output-grams');
+
+
+
+resetButton.style.display = 'none'
+
+
+
+function reset() {
+  resetButton.style.display = 'none';
+  outputMessage.innerHTML = '';
+  urlInput = document.getElementById('url').value = ""
+}
+
+
+
+async function getPollutionURL() {
     try {
-      const response = await fetch('https://api.websitecarbon.com/site?url=https%3A%2F%2Fwww.github.com%2F');
+      const response = await fetch(`https://api.websitecarbon.com/site?url=https%3A%2F%2F${urlInput}%2F`);
+
       if (!response.ok) {
         throw new Error(response.status);
       }
+
       const data = await response.json();
-      return data.url;
+      outputMessage.innerHTML = `${data.url} produces ${data.statistics.co2.grid.grams} grams of CO2 per page load.`;
+
     } catch (error) {
       console.error(error);
+      outputMessage.innerHTML = error;
+      resetButton.style.display = '';
     }
   }
 
-  getPollutionURL()
+
+
+  fetchButton.addEventListener('click', getPollutionURL);
+
+
+
+  resetButton.addEventListener('click', reset)
